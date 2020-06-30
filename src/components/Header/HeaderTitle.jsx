1db998825146f707b2b2ecd36cb58ef264802eb7
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2020-06-30 12:38:59 -0500

    bug redirect home in header is solved

--- src/components/Header/HeaderTitle.jsx
+++ src/components/Header/HeaderTitle.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import {appRoutes} from '../routes/AppRoutes'
+
 const HeaderTitle = () => {
+  const {homePageRoute} = appRoutes
   return (
     <div className="container-headerTitle">
-      <NavLink to="/metodosnumericosInterpolaciones/">
+      <NavLink to={homePageRoute}>
         <h1>Calculadora Interpolaciones</h1>
       </NavLink>
     </div>
